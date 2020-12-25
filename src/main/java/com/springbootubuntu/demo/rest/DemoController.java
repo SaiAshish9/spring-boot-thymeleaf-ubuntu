4148f8fa59c16eb0c2891ce9b23f8ee146611f26
saiashish9 <saiashish3760@gmail.com>
2020-12-26 03:30:31 +0530

    thymeleaf updated

---
+++ src/main/java/com/springbootubuntu/demo/rest/DemoController.java
@@ -0,0 +1,15 @@
+package com.springbootubuntu.demo.rest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DemoController {
+
+	@GetMapping("/hello")
+	public String sayHello(Model theModel) {
+		return "hello";
+	}
+
+}
